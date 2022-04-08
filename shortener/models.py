943221cc65572cd40d82986865b73581f24967c4
Victor Herver <vicherver@gmail.com>
2022-04-07 22:36:31 -0500

    Black added - Url model created to store url to short - Black added to reformat files based in pep8 - Flake8 added - Created requirements.txt file

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,23 @@
+from hashlib import md5
+
 from django.db import models
 
-# Create your models here.
+
+class URL(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    url = models.URLField(unique=True)
+    short = models.URLField(unique=True)
+    clicks = models.IntegerField(default=0)
+
+    def clicked(self):
+        self.clicks += 1
+        self.save(update_fields=["clicks"])
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.url_hash = md5(self.url.encode()).hexdigest()[:10]
+
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.url
