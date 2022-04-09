c1e93edc02ab877847a6fe4d7fcaf4d5a39f93b9
Victor Herver <vicherver@gmail.com>
2022-04-09 17:44:09 -0500

    Added clicked function when retrieve short code

--- shortener/views.py
+++ shortener/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from shortener.serializers import URLSerializer
 from shortener.models import URL
@@ -12,3 +13,9 @@ class ShortenerRetrieveView(generics.RetrieveAPIView):
     serializer_class = URLSerializer
     lookup_field = "short"
     queryset = URL.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.clicked()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
